0e6f5f10c1e1ced3ecdcd880f7af9504e9ac11ec
maxsjodahl <max.sjodahl12@gmail.com>
2026-02-26 00:53:28 +0100

    feat: add docstring-guard pre-commit hook and enhance error reporting

--- src/checker.rs
+++ src/checker.rs
@@ -1,3 +1,5 @@
+use colored::Colorize;
+
 use anyhow::{Context, Result};
 use rustpython_parser::{Mode, ast::Stmt, parse, text_size::TextRange};
 use std::{fs, path::Path};
@@ -8,15 +10,14 @@ pub struct MissingDocstring {
     pub line_number: usize,
 }
 
-fn file_name_to_string(path: &Path) -> String {
-    path.file_name()
-        .map(|f| f.to_string_lossy().to_string())
-        .unwrap_or_else(|| path.to_string_lossy().to_string())
-}
-
 fn load_file_content(path: impl AsRef<Path>) -> Result<String> {
-    fs::read_to_string(path.as_ref())
-        .with_context(|| format!("Failed to read file to string {}", path.as_ref().display()))
+    fs::read_to_string(path.as_ref()).with_context(|| {
+        format!(
+            "{} {} failed to read",
+            format!("{}:", path.as_ref().display()).bold(),
+            "error:".red()
+        )
+    })
 }
 
 fn get_line_number(content: &str, range: TextRange) -> usize {
@@ -33,10 +34,15 @@ fn is_docstring(stmt: &Stmt) -> bool {
 
 pub fn check_file_for_docstrings(file_path: impl AsRef<Path>) -> Result<Vec<MissingDocstring>> {
     let path = file_path.as_ref();
-    let file_name = file_name_to_string(path);
     let content = load_file_content(path)?;
-    let module: rustpython_parser::ast::Mod = parse(&content, Mode::Module, &file_name)
-        .with_context(|| format!("Failed to parse file content from {}", path.display()))?;
+    let module: rustpython_parser::ast::Mod = parse(&content, Mode::Module, "<unknown>")
+        .with_context(|| {
+            format!(
+                "{} {} failed to parse",
+                format!("{}:", path.display()).bold(),
+                "error:".red()
+            )
+        })?;
 
     let mut missing_docstrings: Vec<MissingDocstring> = vec![];
     if let Some(module) = &module.as_module() {
@@ -48,7 +54,7 @@ pub fn check_file_for_docstrings(file_path: impl AsRef<Path>) -> Result<Vec<Miss
                 if let Some(docstring) = &func_stmt.body.first() {
                     if !is_docstring(docstring) {
                         let entry = MissingDocstring {
-                            file_name: file_name.clone(),
+                            file_name: path.display().to_string(),
                             name: id.to_string(),
                             line_number: get_line_number(&content, range),
                         };
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,17 @@
+use anyhow::Error;
+use colored::Colorize;
 use std::env;
 use std::process::exit;
-
-use anyhow::Error;
 
 use crate::checker::{MissingDocstring, check_file_for_docstrings};
 mod checker;
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
-    if args.is_empty() {
-        println!("no *.py file provided");
-        println!("skipping docstring check...");
-        exit(0);
-    }
-
     let mut docstring_errors: Vec<MissingDocstring> = vec![];
     let mut errors: Vec<Error> = vec![];
-    for argv in args {
+
+    for argv in &args {
         match check_file_for_docstrings(argv) {
             Ok(missing_docstrings) => {
                 docstring_errors.extend(missing_docstrings);
@@ -28,18 +23,30 @@ fn main() {
     }
 
     for err in errors.iter() {
-        eprintln!("Error: {err}");
-        eprintln!("Caused by: {}", err.root_cause());
+        eprintln!("{} - {}", err, err.root_cause());
     }
 
     for missing_docstring in docstring_errors.iter() {
         println!(
-            "no docstring in function: '{}' in file: '{}' on line: {}",
-            missing_docstring.name, missing_docstring.file_name, missing_docstring.line_number
+            "{} {} no docstring in function '{}'",
+            format!(
+                "{}:{}:",
+                missing_docstring.file_name, missing_docstring.line_number
+            )
+            .bold(),
+            "failed:".red(),
+            missing_docstring.name,
         );
     }
 
     if !errors.is_empty() || !docstring_errors.is_empty() {
+        println!();
+        // TODO: fix failed files count and passed files in summary
+        println!(
+            "{} {{TODO}} fails, {} errors ({{TODO}} passed)",
+            "docstring-guard:".bold(),
+            errors.len()
+        );
         exit(1)
     }
 
